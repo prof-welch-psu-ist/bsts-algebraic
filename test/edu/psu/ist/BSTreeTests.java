f03546a9cda3e21e1d8a21cda540b36b95dd566a
dtwelch <dtw.welch@gmail.com>
2024-10-31 02:18:41 -0400

    move some tests from UP section over

--- test/edu/psu/ist/BSTreeTests.java
+++ test/edu/psu/ist/BSTreeTests.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 
 public final class BSTreeTests {
 
+    @Test public void testInsert00() {
+        BSTree tr = new Empty(); // "tr" short for "tree"
+        Assertions.assertEquals(0, tr.size());
+    }
+
     @Test public void testInsert01() {
         var tr = BSTree.empty();
         Assertions.assertEquals(0, tr.size());
@@ -40,5 +45,38 @@ public final class BSTreeTests {
                 () -> tr.insert(10) //
                         .insert(5)  //
                         .insert(10));
+    }
+
+
+    @Test public void testInsert02() {
+        BSTree tr = new Empty();
+        tr = tr.insert(10);
+        tr = tr.insert(5);
+        Assertions.assertEquals("10 5", tr.preOrder());
+
+        tr = tr.insert(20);
+
+        Assertions.assertEquals("10 5 20", tr.preOrder());
+        Assertions.assertEquals(3, tr.size());
+    }
+
+    @Test public void testInsertDupFail01() {
+        var tr = new Empty().insert(10);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> tr.insert(10));
+    }
+
+    @Test public void testInsertDupFail02() {
+        var tr = new Empty().insert(10).insert(5);
+        Assertions.assertEquals("10 5", tr.preOrder());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> tr.insert(10));
+    }
+
+    @Test public void testLopsidedTree() {
+        var tr = BSTree.empty()//
+                .insert(10)//
+                .insert(5)//
+                .insert(2)//
+                .insert(0);
+        Assertions.assertEquals("10 5 2 0", tr.preOrder());
     }
 }
