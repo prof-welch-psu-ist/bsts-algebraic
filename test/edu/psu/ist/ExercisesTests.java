598caad75a6cb5577881d17e28d9265de869e3ee
dtwelch <dtw.welch@gmail.com>
2024-10-31 14:59:38 -0400

    a few tests in here already

--- test/edu/psu/ist/ExercisesTests.java
+++ test/edu/psu/ist/ExercisesTests.java
@@ -5,41 +5,42 @@ import org.junit.jupiter.api.Test;
 
 public final class ExercisesTests {
 
-    @Test public void testSum01() {
+
+    /*@Test public void testSum01() {
         var tr = BSTree.empty() //
                 .insert(10) //
                 .insert(5) //
                 .insert(20) //
                 .insert(15); //
         Assertions.assertEquals(50, Exercises.sumAll(tr));
-    }
+    }*/
 
-    @Test public void testSum02() {
+    /*@Test public void testSum02() {
         var tr = BSTree.empty() //
                 .insert(10);
         Assertions.assertEquals(10, Exercises.sumAll(tr));
-    }
+    }*/
 
-    @Test public void testSum03() {
+    /*@Test public void testSum03() {
         var tr = BSTree.empty();
         Assertions.assertEquals(0, Exercises.sumAll(tr));
-    }
+    }*/
 
-    @Test public void testSumLeafs01() {
+    /*@Test public void testSumLeafs01() {
         var tr = BSTree.empty()
                 .insert(10)
                 .insert(5)
                 .insert(20)
                 .insert(15);
         Assertions.assertEquals(20, Exercises.sumLeafs(tr));
-    }
+    }*/
 
-    @Test public void testMaxDepth() {
+    /*@Test public void testMaxDepth() {
         var tr = BSTree.empty()
                 .insert(10)
                 .insert(5)
                 .insert(20)
                 .insert(25);
         Assertions.assertEquals(3, Exercises.maxDepth(tr));
-    }
+    }*/
 }
