2faf950698f59b55d99c5fa5f0241e37c4d51401
dtwelch <dtw.welch@gmail.com>
2024-11-06 18:26:21 -0500

    added some stuff

--- src/edu/psu/ist/Exercises.java
+++ src/edu/psu/ist/Exercises.java
@@ -1,43 +1,86 @@
 package edu.psu.ist;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public final class Exercises {
 
-    /**
-     * Creates a greeting message based on the contents of a binary search tree (BST).
-     *
-     * <p>Uses <a href="https://docs.oracle.com/en/java/javase/17/language/switch-expressions-and-statements.html">Java 17 switch expressions</a> and pattern matching:</p>
-     * <ul>
-     *   <li><strong>Deconstruction:</strong> Breaks down a BST node into its left subtree, data, and right subtree.</li>
-     *   <li><strong>Wildcard (_):</strong> Matches any value but ignores it.</li>
-     * </ul>
-     *
-     * <p>Handled cases:</p>
-     * <ul>
-     *   <li><strong>NonEmpty:</strong> Deconstructs the node to use its data and left subtree.</li>
-     *   <li><strong>Empty:</strong> Matches with a wildcard.</li>
-     * </ul>
-     *
-     * @param t the binary search tree to create the greeting from
-     * @return a greeting string based on the tree's contents
-     *
-     * @see <a href="https://blog.jetbrains.com/idea/2020/09/java-15-and-intellij-idea/">Sealed Interfaces and Records Article</a>
-     * @see <a href="https://docs.oracle.com/en/java/javase/17/language/switch-expressions-and-statements.html">Java Switch Documentation</a>
-     */
-    public static String hello(BSTree t) {
-        String myReturnStr = switch (t) {
-            case NonEmpty(var l, var d, _) ->
-                    "Hello, I'm a BST with data: " + d + "\n" +
-                    "Here's the size of my left subtree: " + l.size();
-            case Empty e -> "Empty :-(";
+    public static int sumAll(BSTree tree) {
+        return switch (tree) {
+            case Empty _ ->  0;
+            case NonEmpty(var l, var data, var r) ->
+                data + sumAll(l) + sumAll(r);
         };
-        return myReturnStr;
     }
 
-    public static void main(String[] args) {
-        var exampleBst = BSTree.empty()//
-                .insert(10) //
-                .insert(5) //
-                .insert(39);
-        System.out.println(hello(exampleBst));
+    //          10
+    //     5         20
+    //                     25
+    public static int maxDepth(BSTree tree) {
+        return switch (tree) {
+            case Empty _                   -> 0;
+            case NonEmpty(var l, _, var r) -> {
+                var depthOfLeft = maxDepth(l);
+                var depthOfRight = maxDepth(r);
+                if (depthOfLeft < depthOfRight) {
+                    yield 1 + depthOfRight;
+                }
+                else {
+                    yield 1 + depthOfLeft;
+                }
+            }
+        };
     }
+
+    //          8
+    //      2
+    public static double average(BSTree tree) {
+        int sum = sumAll(tree);
+        /*if (tree instanceof Empty) {
+            return 0;
+        }
+        if (tree instanceof NonEmpty) {
+            return (double) sum / tree.size();
+        }*/
+        return switch (tree) {
+            case Empty _ -> 0;
+            case NonEmpty t -> (double) (sum / t.size());
+        };
+    }
+
+    public static boolean pathSum(BSTree tree, int target) {
+        return switch (tree) {
+            // case 1: looking at empty trees....
+            case Empty _ when target == 0 -> true;
+            case Empty _                  -> false;
+
+            // case 2: looking at a leaf node....
+            case NonEmpty(Empty _, var d, Empty _) -> target - d == 0;
+
+            // case 3: looking at a nonempty tree
+            //          where the left or right kids might be nonempty
+            case NonEmpty(var l, var d, var r) -> {
+               // two possibilities:
+               //   1) we either take data d from THIS node and subtract it from
+               //       our target amt
+               //   2) we DONT take the data d from THIS node, and still recurse
+                //
+            }
+
+        };
+    }
+
+
+    //              10                                          f(10)
+    //          5        20   === allSatisfy(tree, f) ==> f(5)          f(20)
+    //                15                                            f(15)
+    public static boolean allSatisfy(BSTree tree,
+                                     Predicate<Integer> f) {
+        return switch (tree) {
+            case Empty _                         -> true;
+            case NonEmpty(var lf, var x, var rt) ->
+                f.test(x) && allSatisfy(lf, f) && allSatisfy(rt, f);
+        };
+    }
+
 }
